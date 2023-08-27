b8033f31e51206058e9fbbd982d627678feb7a1f
Graps <twinklierre@gmail.com>
2023-08-27 16:02:32 +0700

    Init

---
+++ simplic/simplic.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <tuple>
+
+#include "simplic.h"
+
+int main(int argc, char* argv[])
+{
+    // cli should look like      smplc.exe 'file-to-compile'
+    if (argc != 2)
+    {
+        std::cout << "Requires 1 argument: filepath" << std::endl;
+        return 1;
+    }
+
+    // start reading the file
+    std::ifstream SourceFile(argv[1]);
+    std::list<std::string> asmlines;
+    std::stringstream buffer;
+
+    if (SourceFile.is_open())
+    {
+        buffer << SourceFile.rdbuf();
+        SourceFile.close();
+    }
+    else
+    {
+        std::cerr << "Cannot open file: " + std::string(argv[1]);
+        return -1;
+    }
+
+    // it's assemblin' time
+    std::string s;
+    while (std::getline(buffer, s, '\n')) {
+        asmlines.push_back(s);
+    }
+
+    //got the assembly, now turn that into machine code
+    std::list<uint16_t> mcCode = Simplic::Asm::ReadAsm(asmlines);
+
+    //remove the file extention to prepare it for .hex
+    std::string targetFilepath;
+    size_t lastdot = std::string(argv[1]).find_last_of(".");
+    if (lastdot == std::string::npos) targetFilepath = argv[1];
+    else targetFilepath = std::string(argv[1]).substr(0, lastdot);
+
+    //write machine code to file
+    Simplic::Asm::WriteToHexFile(targetFilepath + ".hex", mcCode);
+    
+
+    return 0;
+}
